309055785f70458baae9c7ed193e797c791c0b6b
GOYEJIN <yaejingo@gmail.com>
2023-05-12 22:03:27 +0900

    test

---
+++ src/test/java/com/sparta/amenityclonecoding/AmenityCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.amenityclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmenityCloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
