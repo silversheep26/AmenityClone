0e10df7109d297b402b64db82494878a00402cd4
seongyun <kanteluv0417@gmail.com>
2023-05-15 13:52:26 +0900

    feat(spring) : 동적 쿼리를 위한 querydsl 셋팅 및 필터조회 로직 추가

--- src/main/java/com/sparta/amenityclonecoding/repository/ReviewRepository.java
+++ src/main/java/com/sparta/amenityclonecoding/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.sparta.amenityclonecoding.repository;
 import com.sparta.amenityclonecoding.entity.Review;
 import com.sparta.amenityclonecoding.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
