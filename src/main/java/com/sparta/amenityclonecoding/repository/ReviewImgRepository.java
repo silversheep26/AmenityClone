d0f372731d6cd39020787c55acce196d67791885
seongyun <kanteluv0417@gmail.com>
2023-05-16 14:24:35 +0900

    fix(spring) : imgupload api 숙박정보, 리뷰에서도 사용하도록 로직변경

--- src/main/java/com/sparta/amenityclonecoding/repository/ReviewImgRepository.java
+++ src/main/java/com/sparta/amenityclonecoding/repository/ReviewImgRepository.java
@@ -3,6 +3,8 @@ package com.sparta.amenityclonecoding.repository;
 import com.sparta.amenityclonecoding.entity.AmenityImg;
 import com.sparta.amenityclonecoding.entity.ReviewImg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,6 @@ import java.util.List;
 @Repository
 public interface ReviewImgRepository extends JpaRepository<ReviewImg, Long> {
     List<ReviewImg> findReviewImgByReview_ReviewId(Long amenityId);
+    @Query("select count(v) from TB_REVIEWIMG v where v.review.reviewId = :reviewId")
+    Long findImg_AmenityId(@Param("reviewId") Long reviewId);
 }
