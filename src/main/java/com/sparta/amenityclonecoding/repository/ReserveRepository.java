cf0f2720ddb73de4e0092e442691c5a57d3aad09
PYJ <june9152@gmail.com>
2023-05-17 14:16:02 +0900

    예약 여부 확인 코드 추가

--- src/main/java/com/sparta/amenityclonecoding/repository/ReserveRepository.java
+++ src/main/java/com/sparta/amenityclonecoding/repository/ReserveRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,7 +18,16 @@ public interface ReserveRepository extends JpaRepository<Reserve, Long> {
     @Query("select r from TB_RESERVE r where r.reserveStartDate >= :startDat and r.reserveEndDate <= :endDat")
     List<Reserve> chkReserveDat(@Param("startDat") Long startDat, @Param("endDat") Long endDat);
 
-    @Query("select exists (select r from TB_RESERVE r where r.reserveStartDate >= :startDat and r.reserveEndDate <= :endDat and r.amenityId = :amenityId and r.roomId = :roomId ) ")
-    boolean chkReserve(@Param("startDat") String StartDat, @Param("endDat") String endDat, @Param("amenityId") Long amenityId, @Param("roomId") Long roomId);
+//    @Query(value = "select exists (select 1 from TB_RESERVE r where r.reserveStartDate >= :startDat and r.reserveEndDate <= :endDat and r.amenityId = :amenityId and r.roomId = :roomId)", nativeQuery = true)
+//    boolean chkReserve(@Param("startDat") Date startDat, @Param("endDat") Date endDat, @Param("amenityId") Long amenityId, @Param("roomId") Long roomId);
+    @Query("SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END FROM TB_RESERVE r WHERE r.reserveStartDate >= :startDat AND r.reserveEndDate <= :endDat AND r.amenityId = :amenityId AND r.roomId = :roomId")
+    boolean chkReserve(
+            @Param("startDat") String startDat,
+            @Param("endDat") String endDat,
+            @Param("amenityId") Long amenityId,
+            @Param("roomId") Long roomId
+    );
+
+
 
 }
